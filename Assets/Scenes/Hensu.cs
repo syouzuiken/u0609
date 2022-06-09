b259c16e071ea7d65fa48f90780a5fdf8261a857
syouzuiken <laybaekzhang@gmail.com>
2022-06-09 15:16:06 +0900

    Update Hensu.cs

--- Assets/Scenes/Hensu.cs
+++ Assets/Scenes/Hensu.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class Hensu : MonoBehaviour
 {
+    int a = 10;
     // Start is called before the first frame update
-    void Start()
+    public void Proc()
     {
-        int a=10;
         float fw=3.14f;
         string str="‚±‚ñ‚É‚¿‚Í";
         bool bl=true;
 
-        Debug.Log(a);
+        Debug.Log(a+a);
         Debug.Log(fw);
         Debug.Log(str);
         Debug.Log(bl);
+    }
+
+    public void Proc2()
+    {
+        Debug.Log(a);
     }
 
     // Update is called once per frame
